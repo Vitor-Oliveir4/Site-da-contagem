340708fdb567c6640d5eb4cc5dc14e6ac23da13b
Vitor-Oliveir4 <138267466+Vitor-Oliveir4@users.noreply.github.com>
2023-07-07 21:40:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,22 +3,25 @@ function iniciar(){
     let fim = document.getElementById('fim')
     let passo = document.getElementById('passo')
     let res = document.getElementById('res')
+    let i = Number(inicio.value)
+    let f = Number(fim.value)
+    let p = Number(passo.value)
+        
     
     if(inicio.value.length == 0 || fim.value.length == 0 || passo.value.lenght == 0) {
         alert('Algo de errado não está certo! Verifique os dados e tente novamente.')
-    }else {
-        let i = Number(inicio.value)
-        let f = Number(fim.value)
-        let p = Number(passo.value)
-        if (i < f) {
-            for(let x = i; x <= f; x = x + p){
-            res.innerHTML += `${x};  `
-        }    res.innerHTML += `FIM!`
-      }else{
-        for(let x = i; x >= f; x = x-p){
-            res.innerHTML += `${x};  `
-        }    res.innerHTML += `FIM!`
-    }
-    }
+            }else if(i <= f){
+            res.innerHTML = ''
+            
+        for(let x = i; x <= f; x = x + p){
+            res.innerHTML += `${x};`
+        }
+    }else{
+    res.innerHTML = ''
     
+        for(let x = i; x >= f; x = x - p){
+            res.innerHTML += `${x};`
+        }
+    }res.innerHTML += 'FIM!'
 }
+
